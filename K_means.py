69999e26f748cd232293dedf333256e3690433e5
Ka Hung Wong <richard.wkh@gmail.com>
2017-12-22 13:46:45 -0800

    refactor: a K_means class is added.
    All previous functinos in K_means.py is moved to a new class.

--- K_means.py
+++ K_means.py
@@ -8,62 +8,67 @@ Created on Thu Dec 21 11:49:34 2017
 import numpy as np
 from scipy.spatial import distance
 
-MAX_ITERATIONS = 50
-TORR = 1e-5
-
-def get_centroids_from_data(data, k):
-    # to select k centroids from data
-    np.random.seed(0)
-    selected_rows = np.random.randint(0, get_num_observations(data), 
-                                      size = k)
-    return np.array(data)[selected_rows, :]
+class K_means():
+    '''
     
-def get_num_observations(data):
-    return (np.array(data)).shape[0]
+    '''
+    def __init__(self, n_clusters = 2, max_iter = 50, 
+                 tol= 1e-4, random_state = 0):
+        self._max_iter = max_iter
+        self._tol = tol
+        self._n_clusters = n_clusters
+        self._seed = random_state
+        self._labels = None
+        self._centroids = None
+        
+    def _get_centroids_from_data(self, data):
+        # to select k centroids from data
+        np.random.seed(self._seed)
+        selected_rows = np.random.randint(0, 
+                                          self._get_num_observations(data), 
+                                          size = self._n_clusters)
+        self._centroids = np.array(data)[selected_rows, :]
+        
+    def _get_num_observations(self, data):
+        return (np.array(data)).shape[0]
 
-def get_num_features(data):
-    # return the number of features based on input data.
-    data = np.array(data)
-    return data.shape[1]
+    def _get_num_features(self, data):
+        # return the number of features based on input data.
+        data = np.array(data)
+        return data.shape[1]
 
-def get_labels(data, centroids):
-    # to obtain the labels in the dataset based on the input centroids
-    return distance.cdist(data, centroids).argmin(axis = 1)
+    def _get_labels(self, data):
+        # to obtain the labels in the dataset based on the input centroids
+        self._labels = distance.cdist(data, self._centroids).argmin(axis = 1)
 
-def is_centroids_complete(labels, k):
-    return (~np.isin(labels, range(k+1))).sum() == 0
-    
-def get_centroids(data, labels, k):
-    while not is_centroids_complete(labels, k):
-        centroids = get_centroids_from_data(data, k)
-        labels = get_labels(data, centroids)
-    list_centroids = [np.average(data[labels == j, ], axis =0) for j in range(k)]
-    return np.vstack(list_centroids)
+    def _get_centroids(self, data):
+        labels = self._labels
+        list_centroids = [np.average(data[labels == j, ], axis =0) 
+            for j in range(self._n_clusters)]
+        self._centroids = np.vstack(list_centroids)
 
-def should_stop(old_centroids, centroids, iters):
-    if iters > MAX_ITERATIONS:
-        return True
-    else:
-        if old_centroids is None:
-            return False
+    def _should_stop(self, old_centroids, iters):
+        if iters > self._max_iter:
+            return True
         else:
-            return (np.abs(old_centroids - centroids).sum() < TORR)
+            if old_centroids is None:
+                return False
+            else:
+                return (np.abs(old_centroids - self._centroids).sum() < self._tol)
         
-def kmeans(data, k):
-    # initiating centroids, iteration, old_centroids 
-    centroids = get_centroids_from_data(data, k)
-    iters = 0
-    old_centroids = None
-    
-    # running the k means
-    while not should_stop(old_centroids, centroids, iters):
-        # assigning
-        old_centroids = centroids
-        iters += 1
-        print(old_centroids)
-        # calculating the labels and centroids
-        labels = get_labels(data, centroids)
-        centroids = get_centroids(data, labels, k)
+    def fit(self, data):
+        # initiating centroids, iteration, old_centroids 
+        iters = 0
+        old_centroids = self._centroids
+        self._get_centroids_from_data(data)
         
-    # return centorids when the stop condition is true
-    return centroids  
+        # running the k means
+        while not self._should_stop(old_centroids, iters):
+            # assigning
+            old_centroids = self._centroids
+            iters += 1
+            
+            # calculating the labels and centroids
+            self._get_labels(data)
+            self._get_centroids(data)
+        
